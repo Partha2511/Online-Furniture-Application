31c67d38635ce87222567acdfe66093cdb54fe7b
Ankita <ankita.sahu2010@gmail.com>
2022-07-13 17:04:38 +0530

    Account,customer,address

--- OnlineFurnitureStore/src/main/java/com/cg/OFS/model/Account.java
+++ OnlineFurnitureStore/src/main/java/com/cg/OFS/model/Account.java
@@ -1,8 +1,59 @@
 package com.cg.OFS.model;
 
-public class Account {
-	private int accountid;
-	private String accountNo;
-	private String accountBalance;
+import javax.persistence.*;
 
+@Entity
+@Table(name="Account_tbl")
+public class Account {
+	
+	@Id
+	private int accountid;
+	@Column(name="Account Number", length=30)
+	private String accountNo;
+	@Column(name="Account Balance", length=30)
+	private String accountBalance;
+	
+	public Account() {
+		super();
+	}
+
+	public Account(int accountid, String accountNo, String accountBalance) {
+		super();
+		this.accountid = accountid;
+		this.accountNo = accountNo;
+		this.accountBalance = accountBalance;
+	}
+
+	public int getAccountid() {
+		return accountid;
+	}
+
+	public void setAccountid(int accountid) {
+		this.accountid = accountid;
+	}
+
+	public String getAccountNo() {
+		return accountNo;
+	}
+
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+
+	public String getAccountBalance() {
+		return accountBalance;
+	}
+
+	public void setAccountBalance(String accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+
+	@Override
+	public String toString() {
+		return "Account [accountid=" + accountid + ", accountNo=" + accountNo + ", accountBalance=" + accountBalance
+				+ "]";
+	}
+
+
+	
 }
