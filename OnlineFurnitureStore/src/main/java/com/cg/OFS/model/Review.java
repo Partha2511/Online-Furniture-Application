1f88d1776096e29298b6484cd317c690d8fb0324
Ankita <ankita.sahu2010@gmail.com>
2022-07-13 17:08:57 +0530

    class

--- OnlineFurnitureStore/src/main/java/com/cg/OFS/model/Review.java
+++ OnlineFurnitureStore/src/main/java/com/cg/OFS/model/Review.java
@@ -1,8 +1,56 @@
 package com.cg.OFS.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="Review_tbl")
 public class Review {
+	
+	@Id
 	private int reviewId;
+	@Column(name="Description", length=30)
 	private String description;
+	@Column(name="Review Ratng", length=30)
 	private int reviewRating;
+	
+	public Review() {
+		super();
+	}
+
+	public Review(int reviewId, String description, int reviewRating) {
+		super();
+		this.reviewId = reviewId;
+		this.description = description;
+		this.reviewRating = reviewRating;
+	}
+
+	public int getReviewId() {
+		return reviewId;
+	}
+
+	public void setReviewId(int reviewId) {
+		this.reviewId = reviewId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getReviewRating() {
+		return reviewRating;
+	}
+
+	public void setReviewRating(int reviewRating) {
+		this.reviewRating = reviewRating;
+	}
+
+	@Override
+	public String toString() {
+		return "Review [reviewId=" + reviewId + ", description=" + description + ", reviewRating=" + reviewRating + "]";
+	}
 
 }
--- OnlineFurnitureStore/src/main/java/com/cg/OFS/model/User.java
+++ OnlineFurnitureStore/src/main/java/com/cg/OFS/model/User.java
@@ -1,9 +1,67 @@
 package com.cg.OFS.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="User_tbl")
 public class User {
+	
+	@Id
 	private int uid;
+	@Column(name="Username", length=30)
 	private String username;
+	@Column(name="Password", length=30)
 	private String password;
+	@Column(name="Role", length=30)
 	private String role;
+	
+	public User() {
+		super();
+	}
+
+	public User(int uid, String username, String password, String role) {
+		super();
+		this.uid = uid;
+		this.username = username;
+		this.password = password;
+		this.role = role;
+	}
+
+	public int getUid() {
+		return uid;
+	}
+
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "User [uid=" + uid + ", username=" + username + ", password=" + password + ", role=" + role + "]";
+	}
 
 }
