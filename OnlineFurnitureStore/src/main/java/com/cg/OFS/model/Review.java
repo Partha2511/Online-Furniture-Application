92571a78f67773a8100878ad9249b3f49870eda0
Ankita <ankita.sahu2010@gmail.com>
2022-07-13 17:13:40 +0530

    class review user

--- OnlineFurnitureStore/src/main/java/com/cg/OFS/model/Review.java
+++ OnlineFurnitureStore/src/main/java/com/cg/OFS/model/Review.java
@@ -1,66 +1,10 @@
 package com.cg.OFS.model;
 
-<<<<<<< HEAD
-import javax.persistence.*;
-
-@Entity
-@Table(name="Review_tbl")
-public class Review {
-	
-	@Id
-	private int reviewId;
-	@Column(name="Description", length=30)
-	private String description;
-	@Column(name="Review Ratng", length=30)
-	private int reviewRating;
-	
-	public Review() {
-		super();
-	}
-
-	public Review(int reviewId, String description, int reviewRating) {
-		super();
-		this.reviewId = reviewId;
-		this.description = description;
-		this.reviewRating = reviewRating;
-	}
-
-	public int getReviewId() {
-		return reviewId;
-	}
-
-	public void setReviewId(int reviewId) {
-		this.reviewId = reviewId;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public int getReviewRating() {
-		return reviewRating;
-	}
-
-	public void setReviewRating(int reviewRating) {
-		this.reviewRating = reviewRating;
-	}
-
-	@Override
-	public String toString() {
-		return "Review [reviewId=" + reviewId + ", description=" + description + ", reviewRating=" + reviewRating + "]";
-	}
-=======
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
->>>>>>> branch 'main' of https://github.com/Partha2511/Online-Furniture-Application.git
-
 @Entity
 @Table(name="review_tbl")
 public class Review {
