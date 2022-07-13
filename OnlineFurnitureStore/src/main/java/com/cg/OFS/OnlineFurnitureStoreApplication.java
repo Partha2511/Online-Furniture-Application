7c2d8d1b64b643ecfb3867238d3f90e75b10828e
Partha <parthapipu@gmail.com>
2022-07-13 13:59:04 +0530

    First Commit

---
+++ OnlineFurnitureStore/src/main/java/com/cg/OFS/OnlineFurnitureStoreApplication.java
@@ -0,0 +1,13 @@
+package com.cg.OFS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFurnitureStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFurnitureStoreApplication.class, args);
+	}
+
+}
---
+++ OnlineFurnitureStore/src/test/java/com/cg/OFS/OnlineFurnitureStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.OFS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFurnitureStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
