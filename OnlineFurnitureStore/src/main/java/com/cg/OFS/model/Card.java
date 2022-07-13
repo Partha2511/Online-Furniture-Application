a508d8e9f2538afef88c803dc2b1c99181a5cee6
bhoomijaray <bhoomijaray@gmail.com>
2022-07-13 17:21:06 +0530

    Bill,Card updated

--- OnlineFurnitureStore/src/main/java/com/cg/OFS/model/Card.java
+++ OnlineFurnitureStore/src/main/java/com/cg/OFS/model/Card.java
@@ -1,8 +1,18 @@
 package com.cg.OFS.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Card_tbl")
 public class Card {
-	
+	@Id
+	@Column(name="Card_No")
 	private int cardNo;
+	
+	@Column(name="Card_Name")
 	private String cardName;
 	public int getCardNo() {
 		return cardNo;
